8fba8fb881a5194d2b70a746081e4a8b175cb184
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-04-22 18:25:20 -0400

    allow user to delete form instance

--- src/CreateForm.jsx
+++ src/CreateForm.jsx
@@ -1,70 +1,108 @@
 import { useState } from "react";
 import AddedForms from "./addedForms";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CreateForm({ inputData }) {
   const [inputsVisible, setInputVisible] = useState(false);
   const [savedValues, setSavedValues] = useState([]);
+  const [currentValues, setCurrentValues] = useState({ id: null, values: [] });
+  const [editButtonsVisible, setEditButtonsVisible] = useState(false);
 
   const formHeader = inputData[1];
-  const inputElements = (value=[]) => { return inputData[0].map((input) => {
-    return (
-      <label key={input.name}>
-        {input.name}{" "}
-        <input name={input.name} placeholder={input.placeholder} type={input.type} defaultValue={value.pop()}/>
-      </label>
-    );
-  });}
+  const inputElements = (value = []) => {
+    return inputData[0].map((input) => {
+      return (
+        <label key={input.name}>
+          {input.name}{" "}
+          <input
+            name={input.name}
+            placeholder={input.placeholder}
+            type={input.type}
+            defaultValue={value.pop()}
+          />
+        </label>
+      );
+    });
+  };
 
-  const [currentValues, setCurrentValues] = useState({id: null, values: []});
-
-  function newInput(){
+  function newInput() {
     handleInputVisible();
-    setCurrentValues({id: null, values: []});
+    setCurrentValues({ id: null, values: [] });
   }
 
-  function handleInputVisible(){
+  function handleInputVisible() {
     setInputVisible(!inputsVisible);
   }
 
-  function updateCurrentValue(id, updatedValue){
-    setCurrentValues({id, values: updatedValue});
+  function deleteForm(id){
+    const newAddedForms = savedValues.filter(values => values.id !== id);
+    setSavedValues(newAddedForms);
+  }
+
+  function handleEditInputVisible() {
+    setEditButtonsVisible(!editButtonsVisible);
+  }
+
+  function updateCurrentValue(id, updatedValue) {
+    setCurrentValues({ id, values: updatedValue });
+    handleInputVisible();
+    handleEditInputVisible();
     console.log(savedValues);
   }
 
-  function handleForm(e){
+  function handleForm(e) {
     e.preventDefault();
 
     let updated = false;
     const form = e.currentTarget;
     const formData = new FormData(form);
     const inputValue = Array.from(formData.values());
-    const newPossibleValues = savedValues.map(values => {
-      if(values.id === currentValues.id) {
+    const newPossibleValues = savedValues.map((values) => {
+      if (values.id === currentValues.id) {
         updated = true;
-        return {id: values.id, values: inputValue}
+        return { id: values.id, values: inputValue };
       }
       return values;
-    })
-    const newValues = updated ? [...newPossibleValues] : [...savedValues, {id: savedValues.length, values: inputValue}];
+    });
+    const newValues = updated
+      ? [...newPossibleValues]
+      : [...savedValues, { id: uuidv4(), values: inputValue }];
+
 
     setSavedValues(newValues);
-    
 
     handleInputVisible();
-   // setFormInstances([...formInstances, {tab: <div onClick={() => setEditInputVisible(!editInputVisible)} className="tab">{inputValue[0]}</div>, inputElements}]);
-   // {formInstances.map((instance, index) => <Fragment key={index}>{instance.tab}</Fragment>)}
-   // 
+    // setFormInstances([...formInstances, {tab: <div onClick={() => setEditInputVisible(!editInputVisible)} className="tab">{inputValue[0]}</div>, inputElements}]);
+    // {formInstances.map((instance, index) => <Fragment key={index}>{instance.tab}</Fragment>)}
+    //
   }
 
   return (
     <>
-      <form  onSubmit={(e) => handleForm(e)}>
+      <form onSubmit={(e) => handleForm(e)}>
         <h2>{formHeader}</h2>
-        <AddedForms savedValues={savedValues} updateCurrentValue={updateCurrentValue} inputElements={inputElements} visible={handleInputVisible}/>
-        {formHeader !== "Personal Details" && !inputsVisible ? (
-          <button type='button' onClick={newInput} className="add">{"Add"}</button>
-        ) : inputElements([...currentValues.values])}
-        {(inputsVisible || formHeader === "Personal Details") && <button type='submit'>Save</button>}
+        {!inputsVisible && (
+          <AddedForms
+            savedValues={savedValues}
+            updateCurrentValue={updateCurrentValue}
+            inputElements={inputElements}
+            deleteForm={deleteForm}
+          />
+        )}
+        {((!inputsVisible && formHeader !== "Personal Details") || (!inputsVisible && formHeader === "Personal Details" && savedValues.length === 0)) && (
+          <button type="button" onClick={newInput} className="add">
+            {"Add"}
+          </button>
+        )} 
+        {inputsVisible && inputElements([...currentValues.values])}
+        
+        {inputsVisible && (
+            <div className="save-cancel">
+              <button onClick={handleInputVisible}>Cancel</button>
+              <button type="submit">Save</button>
+            </div>
+          )
+        }
       </form>
     </>
   );
--- src/addedForms.jsx
+++ src/addedForms.jsx
@@ -1,12 +1,16 @@
-export default function AddedForms({savedValues, updateCurrentValue, visible}){
+export default function AddedForms({savedValues, updateCurrentValue, deleteForm}){
 
     function editMode(element, value){
        // const inputsFilled = inputElements([...value.toReversed()]);
         console.log(value)
         updateCurrentValue(value.id, [...value.values.toReversed()]);
-        visible();
+    }
+
+    function deleteCurrentForm(e,id){
+        e.stopPropagation();
+        deleteForm(id)
     }
     return savedValues.map((value) => {
-        return <div key={value.values[0]} onClick={(e) => editMode(e, value)} className="tab">{value.values[0]}</div>
+        return <div key={value.values[0]} onClick={(e) => editMode(e, value)} className="tab">{value.values[0]}<button onClick={(e) => deleteCurrentForm(e, value.id)} >D</button></div>
     });
 }
